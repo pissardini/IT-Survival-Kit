3a542121b7009ec46a5f7701362c9f8da5159a83
Rodrigo Pissardini <rodrigo@pissardini.com>
2021-03-28 14:19:04 -0300

    Insert basic html and css

--- js/imc/README.md
+++
@@ -1,2 +0,0 @@
-# IMC
-Cálculo de IMC baseado no código do curso de JavaScript da Alura. 
--- nodejs/chatbot-Telegram/functions.js
+++
@@ -1,5 +0,0 @@
-function sanitizeText(object){
-    return object.toString().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g);
-}
-
-module.exports.sanitizeText=sanitizeText;
